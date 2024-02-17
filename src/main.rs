b3313537d0f281849719040a485e38931cb3d476
Tristan <tristan@gmail.com>
2024-02-16 18:52:14 -0600

    Full Release

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io;
+use std::io::{self, Write};
 
 #[derive(Debug, Clone)]
 struct Row {
@@ -7,6 +7,8 @@ struct Row {
     peg_two: Color,
     peg_three: Color,
     peg_four: Color,
+    peg_black: u8,
+    peg_white: u8,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -46,6 +48,8 @@ fn main() {
         peg_two: crate::Color::random_color(),
         peg_three: crate::Color::random_color(),
         peg_four: crate::Color::random_color(),
+        peg_black: 0,
+        peg_white: 0,
     };
 
     let mut game_board: Vec<Row> = vec![
@@ -54,12 +58,15 @@ fn main() {
             peg_two: Color::Blank,
             peg_three: Color::Blank,
             peg_four: Color::Blank,
+            peg_black: 0,
+            peg_white: 0,
         };
         10
     ];
 
     for i in 0..10 {
-        print_board(&secret_row, &false); // remove after testing, prints the secret key
+        clearscreen::clear().unwrap();
+        // print_board(&secret_row, &false); // remove after testing, prints the secret key
         for i in 0..10 {
             let end = i == 9;
             print_board(&game_board[i], &end);
@@ -70,13 +77,29 @@ fn main() {
         game_board[i].peg_two = get_user_input();
         game_board[i].peg_three = get_user_input();
         game_board[i].peg_four = get_user_input();
+
+        let check: (u8, u8) = check_correctness(&game_board[i], &secret_row);
+
+        game_board[i].peg_black = check.0;
+        game_board[i].peg_white = check.1;
+
         if check_win(&game_board[i], &secret_row) {
-            println!("You Win");
+            println!("You win!");
+            println!("Press any key to continue...");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
             break;
-        } else if true {
-            println!("Change me to a funtion calling to give the correct color/correct space")
         } else if i == 9 {
-            println!("Loose");
+            println!("You lose!");
+            println!("Press any key to continue...");
+            io::stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
             break;
         }
     }
@@ -94,15 +117,18 @@ fn print_board(row: &Row, end: &bool) {
         // prints - for the length of the strings
         print!("-");
     }
-    println!("+"); // prints top left corner
+    print!("+"); // prints top left corner
+    println!("-------------+");
 
     println!(
         // prints the peg colors
-        "| {} | {} | {} | {} |",
+        "| {} | {} | {} | {} | B: {} | W: {} |",
         row.peg_one.as_str(),
         row.peg_two.as_str(),
         row.peg_three.as_str(),
-        row.peg_four.as_str()
+        row.peg_four.as_str(),
+        row.peg_black,
+        row.peg_white
     );
 
     if *end == true {
@@ -110,7 +136,8 @@ fn print_board(row: &Row, end: &bool) {
         for _i in 0..length {
             print!("-");
         }
-        println!("+");
+        print!("+");
+        println!("-------------+");
     }
 }
 
@@ -146,4 +173,40 @@ fn check_win(latest_row: &Row, secret_key: &Row) -> bool {
         return true;
     }
     false
+}
+
+fn check_correctness(latest_row: &Row, secret_key: &Row) -> (u8, u8) {
+    let mut black = 0;
+    let mut white = 0;
+
+    let user_array: [&Color; 4] = [
+        &latest_row.peg_one,
+        &latest_row.peg_two,
+        &latest_row.peg_three,
+        &latest_row.peg_four,
+    ];
+
+    let secret_array: [&Color; 4] = [
+        &secret_key.peg_one,
+        &secret_key.peg_two,
+        &secret_key.peg_three,
+        &secret_key.peg_four,
+    ];
+
+    for i in 0..4 {
+        if user_array[i] == secret_array[i] {
+            black += 1;
+        } else {
+            for j in 0..4 {
+                if user_array[i] == secret_array[j] && i != j {
+                    if secret_array[j] != user_array[j] {
+                        white += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    (black, white)
 }
